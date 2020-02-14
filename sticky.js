ad5ca034126399613661fa6d67385b2e85dc679f
mahounie <haha@tutanota.de>
2020-02-14 12:54:00 +0200

    Create sticky.js

---
+++ sticky.js
@@ -0,0 +1,12 @@
+<script>
+  $(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 116) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("active");
+        }
+    });
+});
+</script>
